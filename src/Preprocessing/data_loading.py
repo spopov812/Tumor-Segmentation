37be109d13ed2511fcc57bb2d36b9c00bc732377
spopov812 <aleksandrspopov812@gmail.com>
2020-04-20 12:53:38 -0400

    Results

--- src/Preprocessing/data_loading.py
+++ src/Preprocessing/data_loading.py
@@ -1,9 +1,10 @@
-import torch, os
+import torch, os, sys
 from torch.utils.data import Dataset, DataLoader
 from Utils.niiparsing import load_nii
 import torch.nn.functional as F
 from glob import glob
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Represents the entire brats dataset that is downloaded. Is manipulated by the 
@@ -19,54 +20,77 @@ class BratsDataset(Dataset):
         self.x = []
         self.y = []
 
-        for filename in glob("C:/Users/Jason/GitHub/Tumor-Segmentation/organized_data/*/*flair.nii.gz"):
+        for i, filename in enumerate(glob("../organized_data/HGG/*flair.nii.gz")):
+            
+            if i == 5:
+                break
+            
+            # sys.stdout.write("\rFile: %d" % (i + 1))
+            # sys.stdout.flush()
+            
             split = filename.split('flair')
             self.extract_features(filename, split[0] + 'seg' + split[1])
 
-        print(len(self.x))
-        print(len(self.y))
+        self.x = np.vstack(self.x)
+        print(self.x.shape)
+        self.x = np.array(self.x).reshape(-1, 6)
+        self.y = np.array(self.y).reshape(-1, 1)
 
     """
-    Uses 3x3 sliding window and 0 padding to go through brain images and extract features using all 9
+    Uses 3x3x3 sliding window and 0 padding to go through brain images and extract features using all 27
     pixels. These features will be the features for only the central pixel.
     """
     def extract_features(self, x_path, y_path):
 
         # Loading and normalizing whole brain images
-        x = load_nii(x_path)
-        x = x / np.linalg.norm(x)
-        y = load_nii(y_path)
+        x_arr = load_nii(x_path)
+        x_arr = x_arr / x_arr.max()
+        y_arr = load_nii(y_path)
+        
+        windows = []
+        
+        num_c = 0
+        num_nc = 0
 
-        # Padding
-        x_arr = np.pad(x, ((1, 1), (1, 1), (0, 0)), mode='constant')
-        y_arr = np.pad(y, ((1, 1), (1, 1), (0, 0)), mode='constant')
-
-        assert x_arr.shape[0] == x_arr.shape[1]
-
-        # For each 2D slice
-        for depth in range(x_arr.shape[2]):
+        for depth in range(0, x_arr.shape[2] - 2, 3):
 
             # Parameterizing sliding window
-            for height in range(x_arr.shape[0] - 2):
-                for width in range(x_arr.shape[1] - 2):
-
-                    feature_vec = []
+            for row in range(0, x_arr.shape[0], 3):
+                for col in range(0, x_arr.shape[1], 3):
+                    
+                    if x_arr[row + 1, col + 1, depth + 1] == 0:
+                        num_c += 1
+                        continue
+                    else:
+                        num_nc += 1
 
                     # Creating sliding window across slice
-                    x_window = x_arr[height : height + 3, width : width + 3, depth]
-                    y_window = y_arr[height + 1, width + 1, depth]
-                
-                    # Feature extraction
-                    feature_vec.append(np.mean(x_window))
-                    feature_vec.append(np.std(x_window))
-                    feature_vec.append(np.var(x_window))
+                    x_window = x_arr[row : row + 3, col : col + 3, depth : depth + 3]
+                    y_window = y_arr[row + 1, 
+                                     col + 1, 
+                                     depth + 1]
 
-                    self.x.append(feature_vec)
+                    # Flattening window
+                    windows.append(np.array(x_window).reshape(-1))         
 
+                    # Extracting label for central pixel
                     if y_window == 0:
                         self.y.append([0])
                     else:
                         self.y.append([1])
+
+        windows = np.array(windows).reshape(-1, 27)
+        
+        inp = np.concatenate((
+                             np.mean(windows, axis=1).reshape(-1, 1), 
+                             np.std(windows, axis=1).reshape(-1, 1), 
+                             np.var(windows, axis=1).reshape(-1, 1),
+                             np.max(windows, axis=1).reshape(-1, 1),
+                             np.min(windows, axis=1).reshape(-1, 1),
+                             np.median(windows, axis=1).reshape(-1, 1)), axis=1)
+        
+        self.x.append(inp)
+
             
     """
     Number of samples that have been downloaded.
